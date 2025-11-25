9ae630167970603a3e585654f9a91bfffba0b3e5
Tejas Singh <tejassingh74823@gmail.com>
2025-11-25 22:25:01 +0530

    Compound Interest

---
+++ compound_interest.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    float principal_amount, rate, interest, alpha;		//Compound interest
+    int time;
+    printf("Enter principal amount (in Rs.): ");
+    scanf("%f", &principal_amount);
+    printf("Enter rate of interest: ");
+    scanf("%f",&rate);
+    printf("Enter the number of years: ");
+    scanf("%d",&time);
+    alpha=pow((1+(rate/100.0)),time);
+    interest=principal_amount*(alpha-1);
+    printf("The compound interest for %.2f for %d years at the rate of %.2f %% per annum is: %.2f Rs.",principal_amount, time, rate, interest);
+}
