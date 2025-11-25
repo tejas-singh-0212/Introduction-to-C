b9670fc6f0c99b3cd7c586ba0e5d7844d932b544
Tejas Singh <tejassingh74823@gmail.com>
2025-11-25 16:58:59 +0530

    Added sequential pattern printing

---
+++ sequential_pattern.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main() 
+{
+    int n, i, j;        //Sequential pattern printing program
+    int num = 1; 
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++) 
+    {
+        for (j = 1; j <= i; j++) 
+        {
+            printf("%d", num);
+            num++;
+            if (j != i) 
+            {
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+}
