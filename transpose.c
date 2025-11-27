52b15fe63147e8bf192c56ea25088f546737553c
Tejas Singh <tejassingh74823@gmail.com>
2025-11-27 07:14:42 +0530

    Program to calculate transpose of a user input 3x3 matrix

---
+++ transpose.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void main()
+{
+    int a[3][3];        //Taking matrix elements as input
+    printf("Enter elements of 3x3 matrix:\n");
+    for(int i=0;i<3;i++){ 
+        for(int j=0;j<3;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    int i,j,b[3][3];
+    printf("Original Matrix:\n");       //Printing original matrix
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+
+    for(i=0;i<3;i++){           //Calculating transpose
+        for(j=0;j<3;j++){
+            b[j][i]=a[i][j];
+        }
+    }
+
+    printf("Transpose of Matrix:\n");       //Printing transposed matrix
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            printf("%d\t",b[i][j]);
+        }
+        printf("\n");
+    }
+}
