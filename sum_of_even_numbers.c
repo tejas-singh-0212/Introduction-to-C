c9b63f8eb015d81cad89f1275c493deea227a156
Tejas Singh <tejassingh74823@gmail.com>
2025-12-05 00:17:01 +0530

    Calculate sum of all even numbers in a given range

---
+++ sum_of_even_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int n, sum=0;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        if(i%2==0)
+        {
+            sum=sum+i;
+        }
+    }
+    printf("Sum of even numbers up to %d is: %d\n", n, sum);
+}
