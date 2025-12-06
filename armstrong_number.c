5f8cd9bad0a0bc23683fa95b358f8050f74e7637
Tejas Singh <tejassingh74823@gmail.com>
2025-12-06 12:43:34 +0530

    Fixed armstrong number

--- armstrong_number.c
+++ armstrong_number.c
@@ -1,23 +1,24 @@
-#include<stdio.h>
-void main()
-{
-    int num,remainder,flag=0,o_num;		//Armstrong number
-    printf("Enter the number to be checked: ");
+#include <stdio.h>
+#include <math.h>
+void main() {
+    int num, o_num, remain, digits = 0;
+    double sum = 0;
+    printf("Enter a number: ");
     scanf("%d", &num);
-    o_num=num;
-    while (num != 0) 
-	{
-        remainder = num % 10;
-        flag+=remainder*remainder*remainder;
-        num /= 10;
+    o_num = num;
+    while (o_num != 0) {
+        digits++;
+        o_num /= 10;
     }
-    
-	if(flag==o_num)
-    {
-    	printf("%d is an Armstrong number.",o_num);
-	}
-	else
-	{
-		printf("%d isn\'t an Armstrong number.",o_num);
-	}
+    o_num = num;
+    while (o_num != 0) {
+        remain = o_num % 10;
+        sum += pow(remain, digits);
+        o_num /= 10;
+    }
+    if (sum == num) {
+        printf("%d is an Armstrong number.\n", num);
+    } else {
+        printf("%d is not an Armstrong number.\n", num);
+    }
 }
