9b42d1515cf6b425be9b51ae563b265dc81b48fc
Tejas Singh <tejassingh74823@gmail.com>
2025-12-06 15:42:42 +0530

    Fixed Transpose of matrix

--- transpose.c
+++ transpose.c
@@ -5,6 +5,7 @@ void main()
     printf("Enter elements of 3x3 matrix:\n");
     for(int i=0;i<3;i++){ 
         for(int j=0;j<3;j++){
+            printf("Element [%d][%d]: ", i, j);
             scanf("%d",&a[i][j]);
         }
     }
