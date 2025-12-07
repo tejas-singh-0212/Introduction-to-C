7ac316530180c992f1632bc7d238734e36073f48
Tejas Singh <tejassingh74823@gmail.com>
2025-12-07 13:14:49 +0530

    Insertion Sort algorithm

---
+++ insertion_sort.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void insertion_sort(int arr[],int n) {
+    for(int i=1;i<n;i++) {
+        int key=arr[i];
+        int j=i-1;
+        while(j>=0 && arr[j]>key) {
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+}
+void main()
+{
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d",&n);
+    int arr[n];
+    for (int i=0;i<n;i++) {
+        printf("Enter element %d: ",i+1);
+        scanf("%d",&arr[i]);
+    }
+    int s=sizeof(arr)/sizeof(arr[0]);
+    printf("Unsorted array: \n");
+    for(int i=0;i<s;i++) {
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+    insertion_sort(arr,s);
+    printf("Sorted array: \n");
+    for(int i=0;i<s;i++) {
+        printf("%d ",arr[i]);
+    }
+}
