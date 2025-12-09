e289f7ff1d10e955d8b8518f821abab6758430f1
Tejas Singh <tejassingh74823@gmail.com>
2025-12-09 10:09:03 +0530

    Alternate 0 and 1 triangle pattern

---
+++ 1_0_triangle_pattern.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+    int num = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            if ((i+j)%2==0){
+                printf("1 ");
+            }
+            else{
+                printf("0 ");
+            }
+        }
+        printf("\n");
+    }
+}
