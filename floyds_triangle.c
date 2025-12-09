0d3115bc0d18bf1be648c8995d9bb5a1a3fbd0dd
Tejas Singh <tejassingh74823@gmail.com>
2025-12-09 10:03:51 +0530

    Floyd's Triangle

---
+++ floyds_triangle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("Enter the number of rows: ");
+    scanf("%d", &n);
+    int num = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d ", num++);
+        }
+        printf("\n");
+    }
+}
