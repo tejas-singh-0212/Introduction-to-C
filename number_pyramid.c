7c368aca5c9ca108e07f9dbeafc63cc74a10a907
Tejas Singh <tejassingh74823@gmail.com>
2025-12-10 12:27:55 +0530

    Number Pyramid

---
+++ number_pyramid.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int n;
+    printf("Enter the number of rows: ");
+    scanf("%d",&n);
+    int no_of_star=1;
+    for(int i=1;i<=n;i++){
+        for(int k=1;k<=n-i;k++){
+            printf("  ");
+        }for(int j=1;j<=no_of_star;j++){
+            printf("%d ",j);
+        }
+        no_of_star += 2;
+        printf("\n");
+    }
+}
