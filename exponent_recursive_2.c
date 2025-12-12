7229caea4ab7dbe58e0b837d5b06f54d61d6324d
Tejas Singh <tejassingh74823@gmail.com>
2025-12-12 22:54:42 +0530

    cd

---
+++ exponent_recursive_2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int powerlog(int base, int exp){         //this one makes less number of calls
+    if(exp==0) return 1;
+    int a = powerlog(base,exp/2);
+    if(exp%2==0) return a*a;
+    else return base*a*a;
+}
+void main()
+{
+    int base, exp;
+    printf("Enter base: ");
+    scanf("%d",&base);
+    printf("Enter exponent: ");
+    scanf("%d",&exp);
+    printf("%d raised to the power %d is %d",base,exp,powerlog(base,exp));
+}
