6ca09db13af28fef29d34b641b965e02b701c500
Tejas Singh <tejassingh74823@gmail.com>
2025-12-20 15:09:29 +0530

    To find duplicate element in array

---
+++ only_duplicate_element.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main(){
+    int n;
+    printf("Enter length of array: ");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter elements of array:\n");
+    for (int i=0; i<n; i++){
+        printf("Enter element number %d: ",i+1);
+        scanf("%d",arr+i);
+    }
+    for(int i=0; i<n; i++){
+        for (int j=i+1; j<n; j++){
+            if(arr[i] == arr[j]){
+                printf("%d is the duplicate element.",arr[i]);
+                break;
+            }
+        }
+    }
+}
