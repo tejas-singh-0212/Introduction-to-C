a05c8a92ed2525a8d0a6a8276f553479d934b91e
Tejas Singh <tejassingh74823@gmail.com>
2025-12-26 15:02:02 +0530

    Product of two matrices

--- product_of_matrices.c
+++ product_of_matrices.c
@@ -1,31 +1,56 @@
 #include<stdio.h>
 void main()
 {
-    int a[3][3];                        //mat_1[m][n]
-    printf("Enter elements of 3x3 matrix:\n");      //Input 3x3 matrix elements
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            scanf("%d",&a[i][j]);
-        }
-    }
-    
-    int b[3][3];                        //mat_2[n][p]
-    printf("Enter elements of 3x3 matrix:\n");      //Input 3x3 matrix elements
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            scanf("%d",&b[i][j]);
-        }
-    }      
-
-    int prod[3][3]={{0,0,0},{0,0,0},{0,0,0}};       //prod[m,p]
-    int i,j,k;
-    for(i=0;i<3;i++){       //i<m
-        for(j=0;j<3;j++){       //i<p
-            for(k=0;k<3;k++){       //k<n
-                prod[i][j]+=mat_1[j][k]*mat_2[k][j];
+    int row_1,col_1,row_2,col_2;
+    printf("Enter number of rows of first matrix: ");
+    scanf("%d",&row_1);
+    printf("Enter number of columns of first matrix: ");
+    scanf("%d",&col_1);
+    printf("Enter number of rows of second matrix: ");
+    scanf("%d",&row_2);
+    printf("Enter number of columns of second matrix: ");
+    scanf("%d",&col_2);
+    if(col_1==row_2){
+        int a[row_1][col_1], b[row_2][col_2];
+        //Taking elements of first matrix
+        printf("Enter elements of first matrix:\n");
+        for(int i=0; i<row_1; i++){
+            for(int k=0; k<col_1; k++){
+                printf("Element [%d][%d]: ",i,k);
+                scanf("%d", &a[i][k]);
             }
-            printf("%d\t",prod[i][j]);
         }
-        printf("\n");
+        //Taking elements of second matrix
+        printf("Enter elements of second matrix:\n");
+        for(int i=0; i<row_2; i++){
+            for(int k=0; k<col_2; k++){
+                printf("Element [%d][%d]: ",i,k);
+                scanf("%d", &b[i][k]);
+            }
+        }
+        //declaring product matrix with all elements as zero
+        int product[row_1][col_2];
+        for(int i=0; i<row_1; i++){
+            for(int k=0; k<col_2; k++){
+                product[i][k]=0;
+            }
+        }
+        //multiplying matrices
+        for(int i=0; i<row_1; i++){
+            for(int k=0; k<col_2; k++){
+                for(int l=0; l<col_1; l++){
+                    product[i][k] += a[i][l]*b[l][k];
+                }
+            }
+        }
+        //displaying product
+        printf("The product matrix is:\n");
+        for(int i=0; i<row_1; i++){
+            for(int k=0; k<col_2; k++){
+                printf("%d ",product[i][k]);
+            }printf("\n");
+        }
+    }else{
+        printf("Matrices with these rows and columns cannot be multiplied");
     }
 }
