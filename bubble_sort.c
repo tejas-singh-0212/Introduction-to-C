71a13e3a2b850dbb30e7a4e99eb8c7e4ddbcc502
Tejas Singh <tejassingh74823@gmail.com>
2025-12-27 20:15:30 +0530

    Bubble Sort (Optimized)

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
+#include<stdbool.h>
 void bubble_sort(int arr[],int n) {
-    for(int i=0;i<n-1;i++) {
-        for(int j=0;j<n-i-1;j++) {
-            if(arr[j]>arr[j+1]) {
-                int temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
+    for(int i=0; i<n-1; i++) {
+        bool flag = true;
+        for(int j=0; j<n-i-1; j++) {
+            if(arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                flag = false;
             }
         }
     }
 }
-void main()
-{
+void main(){
     int n;
     printf("Enter number of elements: ");
     scanf("%d",&n);
