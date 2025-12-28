1713708c3903b502d94de930c6d3631cd008990b
Tejas Singh <tejassingh74823@gmail.com>
2025-12-28 10:59:57 +0530

    Insertion Sort Algorithm

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,14 +1,19 @@
 #include<stdio.h>
+void swap(int *a,int *b) {
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+    return;
+}
 void insertion_sort(int arr[],int n) {
-    for(int i=1;i<n;i++) {
-        int key=arr[i];
-        int j=i-1;
-        while(j>=0 && arr[j]>key) {
-            arr[j+1]=arr[j];
+    for(int i=1; i<n; i++) {
+        int j=i;
+        while(j>=1 && arr[j]<arr[j-1]) {
+            swap(arr+j,arr+j-1);
             j--;
         }
-        arr[j+1]=key;
     }
+    return;
 }
 void main(){
     int n;
