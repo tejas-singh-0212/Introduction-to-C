7b5bd86eadcde367217679024febf586a0e5be1d
Tejas Singh <tejassingh74823@gmail.com>
2026-04-09 11:52:27 +0530

    update: added category wise submodules

--- celsius_to_fahrenheit.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    float C, F;
-    printf("Enter temperature in Celsius: ");
-    scanf("%f", &C);
-    F = (C * 9/5) + 32;
-    printf("Equivalent temperature in Fahrenheit: %.1f F\n", F);
-}
